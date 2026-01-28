ca56eb955acafc630391d9880ae9c982f068735a
Leonardo Caetano <129470226+leocaetano7@users.noreply.github.com>
2026-01-28 01:53:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function foge(botao) {
+    // 1. Limita as bordas da tela
+    const larguraJanela = window.innerWidth;
+    const alturaJanela = window.innerHeight;
+
+    // 2. Dimensões do botão
+    const larguraBotao = botao.offsetWidth;
+    const alturaBotao = botao.offsetHeight;
+
+    // 3. Area máxima que o botão pode ocupar tela e botão
+    const maxX = larguraJanela - larguraBotao - 20;
+    const maxY = alturaJanela - alturaBotao - 20;
+
+    // 4. Gerar posição aleatoria
+    const novaPosX = Math.max(10, Math.random() * maxX);
+    const novaPosY = Math.max(10, Math.random() * maxY);
+
+    botao.style.position = "fixed";
+    botao.style.left = novaPosX + "px";
+    botao.style.top = novaPosY + "px";
+}
+
+function aceitou() {
+    alert("Sábia escolha! ❤️ Agora você terá que me aguentar para sempre!");
+    
+    window.location.href = "https://www.youtube.com/watch?v=fGPcm1I7U90";
+}
